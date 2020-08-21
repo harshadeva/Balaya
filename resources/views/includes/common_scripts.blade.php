9c8d788a1ffc83e18dfdccf48067c0513e3cb18f
harshadeva <hpbandara94@gmail.com>
2020-08-21 08:54:28 +0530

    with Google Analytics

--- resources/views/includes/common_scripts.blade.php
+++ resources/views/includes/common_scripts.blade.php
@@ -152,6 +152,16 @@
         });
     });
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154895234-4"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-154895234-4');
+</script>
+
 
 
 
